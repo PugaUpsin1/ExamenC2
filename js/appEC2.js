e100b12b3e4818ac24ffbae596cbf065eb61122a
PugaUpsin1 <2020030282@upsin.edu.mx>
2022-10-27 20:42:03 -0600

    Codificación del botón calcular

--- js/appEC2.js
+++ js/appEC2.js
@@ -29,5 +29,30 @@ function descarte(){
     }
 }
 
-
+function calcularcam() {
+    let cant = document.getElementById('cant').value;
+    let origen = document.getElementById('origen').value;
+    let destino = document.getElementById('destino').value;
+    console.log(cant);
+    
+    cambio(cant, origen, destino)
+    function cambio(cant, origen, destino){
+        let sub = 0;
+        if(origen == 19.85){
+            sub = cant / 19.85 * destino;
+        }else if(origen == 1){
+            sub = cant / 1 * destino;
+        }else if(origen == 1.35){
+            sub = cant / 1.35 * destino;
+        }
+        else if(origen == .99){
+            sub = cant / .99* destino;
+        }
+        document.getElementById('subtot').value = sub.toFixed(2);
+        let comi = sub * 0.03; 
+        document.getElementById('comi').value = comi.toFixed(2);
+        let total = sub + comi;
+        document.getElementById('tot').value = total.toFixed(2);
+    }
+}
 
